c28e3de76825ca17bc40aeb6a9dcddc615805c46
Jose Paez <josefinapaezdevalle@gmail.com>
2022-12-19 19:25:18 -0300

    cambios

--- Js/main.js
+++ Js/main.js
@@ -1,4 +1,4 @@
-class producto {
+class Producto {
     constructor(producto,precio,talle,cantidad){
         this.producto = producto;
         this.precio = parseFloat (precio);
@@ -13,23 +13,26 @@ class producto {
         this.disponible = false;
     }
 }
-var arrayProductos = [];
+let arrayProductos = [];
 do{
-    var comprobacion = prompt ('Ingresar nombre del producto');
+    let comprobacion = prompt ('Ingresar nombre del producto');
     if (comprobacion === "listo" ||comprobacion === "LISTO" ||comprobacion === "Listo"){
         break;
     }else{
         nombreProd = comprobacion;
-        var precioProd =prompt ('Ingresar precio de producto')
-        var talleProd =prompt ('Ingresar talle de producto')
-        var cantidadProd =prompt ('Ingresar cantidad de productos')
+        let precioProd =prompt ('Ingresar precio de producto')
+        let talleProd =prompt ('Ingresar talle de producto')
+        let cantidadProd =prompt ('Ingresar cantidad de productos')
     }
 }
 while (comprobacion != "listo"|| comprobacion != "LISTO" || comprobacion != "Listo")
 
 console.log(arrayProductos);
+for (let arrayProductos = 0; arrayProductos < 5; arrayProductos++) {
+    console.log('Cinco veces');
+}
 
-for (var producto of arrayProductos){
+for (let Producto of arrayProductos){
     document.write("<ul><li><h3>Producto: " + producto.producto + "</h3></li></ul>");
     document.write("<li><h3>Talle: " + producto.talle + "</h3></li>");
     document.write("<li><h3>Cantidad: " + producto.cantidad + "</h3></li>");
@@ -41,21 +44,21 @@ for (var producto of arrayProductos){
 }
 
 
-var sinStock = arrayProductos.filter(producto => producto.cantidad == 0 || producto.disponible == false);
+let sinStock = arrayProductos.filter(producto => producto.cantidad == 0 || producto.disponible == false);
 console.log(sinStock);
 document.write("<h3>Sin stock</h3>");
 
-for (var producto of sinStock){
+for (let producto of sinStock){
     document.write("<ul><li><h3>Producto: " + producto.producto + "</h3></li></ul>");
     document.write("<li><h3>Talle: " + producto.talle + "</h3></li>");
     document.write("<li><h3>Precio: " + producto.cantidad + "</h3></li>"); 
-}
-var ingresado = prompt('Buscar producto');
-var prodIngresado = arrayProductos.filter(producto => producto.producto.includes(ingresado));
+let ingresado = prompt('Buscar producto');
+let prodIngresado = arrayProductos.filter(producto => producto.producto.includes(ingresado));
 console.log(prodIngresado);
 document.write("<h2>Busqueda de productos</h2>");
+}
 
-for (var producto of prodIngresado){
+for (let producto of prodIngresado){
     document.write("<ul><li><h3>Producto: " + producto.producto + "</h3></li></ul>");
     document.write("<li><h3>Talle: " + producto.talle + "</h3></li>");
     document.write("<li><h3>Precio: " + producto.precio + "</h3></li>"); 
